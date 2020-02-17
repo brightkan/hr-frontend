3f6df85eb2e1fae1ec3ff5e8eed4692e9e1f9cce
Bright Kanyange <brightkanyange@outlook.com>
2020-02-17 20:00:32 +0300

    Done with login page

---
+++ src/pages/login/widgets/LoginForm.js
@@ -0,0 +1,61 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+const LoginForm = () => {
+  const classes = useStyles();
+  return (
+    <form method="POST" action="#">
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        id="email"
+        label="Email Address"
+        name="email"
+        autoComplete="email"
+        autoFocus
+      />
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+      />
+
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="secondary"
+        className={classes.submit}
+      >
+        LOGIN
+      </Button>
+    </form>
+  );
+};
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
+
+export default LoginForm;
---
+++ src/pages/login/widgets/Logo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Logo = () => {
+  return <img style={{ width: "50%" }} src="logo.svg" alt="logo" />;
+};
+
+export default Logo;
